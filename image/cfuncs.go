0905cf6bb1db51f2499e86bb343401cfbbecec64
DESKTOP-LR44U6B\rbist <i.rbouh@gravitom.ma>
2022-02-07 01:01:09 +0100

    fix extren functions

--- image/cfuncs.go
+++ image/cfuncs.go
@@ -4,10 +4,10 @@ package image
 #include <wkhtmltox/image.h>
 #include <stdio.h>
 
-extern void progress_changed(*wkhtmltoimage_converter, int);
-extern void phase_changed(*wkhtmltoimage_converter, int);
-extern void set_error(*wkhtmltoimage_converter, int);
-extern void set_warning(*wkhtmltoimage_converter, int);
+extern void progress_changed(wkhtmltoimage_converter*, int);
+extern void phase_changed(wkhtmltoimage_converter*, int);
+extern void set_error(wkhtmltoimage_converter*, int);
+extern void set_warning(wkhtmltoimage_converter*, int);
 
 void progress_changed_cgo(wkhtmltoimage_converter *converter, int p) {
   progress_changed(converter, p);
